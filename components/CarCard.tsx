1dbb87e27070627c334d60dbe8162a48c83e0775
GabrielOchieng <ogingagabriel@gmail.com>
2024-01-30 11:40:17 +0300

    Update

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 import Image from "next/image";
 import { CarProps } from "@/types";
 import { calculateCarRent } from "@/utils";
-import { CustomButton } from ".";
+import { CarDetails, CustomButton } from ".";
 
 interface CarCardProps {
   car: CarProps;
@@ -71,6 +71,11 @@ const CarCard = ({ car }: CarCardProps) => {
           />
         </div>
       </div>
+      <CarDetails
+        isOpen={isOpen}
+        closeModal={() => setIsOpen(false)}
+        car={car}
+      />
     </div>
   );
 };
